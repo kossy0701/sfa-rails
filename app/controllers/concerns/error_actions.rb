1a25641ddbdb809da9059dc6f0c445419cace1fb
kossy0701 <0421kossy0421@gmail.com>
2020-06-17 20:31:26 +0900

    add-error-handler

---
+++ app/controllers/concerns/error_actions.rb
@@ -0,0 +1,18 @@
+module ErrorActions
+  extend ActiveSupport::Concern
+
+  def error_json(exception)
+    {
+      type:       'error',
+      status:     Rack::Utils.status_code(exception.status),
+      code:       exception.code,
+      message:    exception.message,
+      errors:     exception.errors,
+      info:       exception.info
+    }.to_json
+  end
+
+  def render_error(exception)
+    render json: error_json(exception), status: exception.status
+  end
+end
