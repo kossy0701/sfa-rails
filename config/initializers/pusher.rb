3abae9e30cbd048f24f0df8fad029e0f454bbe02
kossy0701 <0421kossy0421@gmail.com>
2020-12-19 15:46:56 +0900

    Update pusher.rb

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,8 +1,10 @@
 require 'pusher'
 
-Pusher.app_id = Rails.application.credentials.pusher[:app_id]
-Pusher.key = Rails.application.credentials.pusher[:key]
-Pusher.secret = Rails.application.credentials.pusher[:secret]
-Pusher.cluster = Rails.application.credentials.pusher[:cluster]
-Pusher.logger = Rails.logger
-Pusher.encrypted = true
+unless Rails.env.test?
+ Pusher.app_id = Rails.application.credentials.pusher[:app_id]
+ Pusher.key = Rails.application.credentials.pusher[:key]
+ Pusher.secret = Rails.application.credentials.pusher[:secret]
+ Pusher.cluster = Rails.application.credentials.pusher[:cluster]
+ Pusher.logger = Rails.logger
+ Pusher.encrypted = true
+end
