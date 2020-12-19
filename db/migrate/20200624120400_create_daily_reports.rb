b367bb19abc60ca76d658e53f7ef4f70a968d76b
kossy0701 <0421kossy0421@gmail.com>
2020-12-19 15:47:17 +0900

    add status for DailyReport Model

--- db/migrate/20200624120400_create_daily_reports.rb
+++ db/migrate/20200624120400_create_daily_reports.rb
@@ -5,6 +5,7 @@ class CreateDailyReports < ActiveRecord::Migration[6.0]
       t.string :problem
       t.string :improvement
       t.string :consultation
+      t.integer :status, null: false, default: 1
 
       t.timestamps
     end
