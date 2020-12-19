7842e25ad388380bbebab46acd419990cb72eebb
kossy0701 <0421kossy0421@gmail.com>
2020-12-19 13:57:31 +0900

    add enum status in DailyReport Model

--- app/models/daily_report.rb
+++ app/models/daily_report.rb
@@ -1,7 +1,10 @@
 class DailyReport < ApplicationRecord
   belongs_to :user
 
+  enum status: %i[draft open only_manager]
+
   validates :problem, length: { maximum: 255 }
   validates :improvement, length: { maximum: 255 }
   validates :consultation, length: { maximum: 255 }
+
 end
